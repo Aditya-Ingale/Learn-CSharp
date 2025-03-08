13c375c86cfb120389b4336099cf59b3b3c6780d
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-03-08 17:10:03 +0530

    Input Output Code

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -95,3 +95,39 @@ namespace oddEven
 */
 
 
+// Input and Output 
+/*
+namespace ConsoleID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello my name is Adi!");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            //Console.Write("Enter your age: ");
+            //string ageInput = Console.ReadLine();
+            //int age = Convert.ToInt32(ageInput);
+            ////Console.WriteLine(age);
+
+            Console.Write("Enter your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Your name is ");
+            //Console.Write(name);
+            //Console.Write(" and your age is ");
+            //Console.Write(age);
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
