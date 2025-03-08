b3fcab389a5b23fa9792ee75a1a02fbef5200636
Aditya-Ingale <adityaa.ingale@gmail.com>
2025-03-08 16:51:20 +0530

    Modified Code

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Modulus
+// Modulus 
+/* namespace Modulus
 {
     class Program
     {
@@ -23,3 +25,73 @@ namespace Modulus
         }
     }
 }
+*/
+
+//Constants Keyword
+/*
+ * namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int vat = 20;
+            const double percentVAT = vat / 100D;
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (vat / 100D));
+            Console.WriteLine(balance * percentVAT);
+            Console.WriteLine(vat);
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
+// Storing User Data
+/*
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Adi";
+            var number = 1234567892;
+            var age = 23;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
+//odd of even checker
+/*
+namespace oddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+            num1 = 11;
+            remainder = num1 % num2;
+            Console.WriteLine(remainder);
+        }
+    }
+}
+*/
+
+
